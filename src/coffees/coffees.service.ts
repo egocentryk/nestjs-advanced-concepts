3063379ed25aff229941fb5c03fcd0f50d7794ca
egocentryk <lukasz.skowron@gmail.com>
2025-03-05 15:24:22 +0100

    feat(): module lazy-loading implemented

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -2,6 +2,7 @@ import { Inject, Injectable } from '@nestjs/common'
 import { CreateCoffeeDto } from './dto/create-coffee.dto'
 import { UpdateCoffeeDto } from './dto/update-coffee.dto'
 import { Coffee } from './entities/coffee.entity'
+import { LazyModuleLoader } from '@nestjs/core'
 
 export const COFFEES_DATA_SOURCE = Symbol('COFFEES_DATA_SOURCE')
 
@@ -11,9 +12,22 @@ export interface CoffeesDataSource {
 
 @Injectable()
 export class CoffeesService {
-  constructor(@Inject(COFFEES_DATA_SOURCE) dataSource: CoffeesDataSource) {}
+  constructor(
+    @Inject(COFFEES_DATA_SOURCE) dataSource: CoffeesDataSource,
+    private readonly lazyModuleLoader: LazyModuleLoader,
+  ) {}
 
-  create(createCoffeeDto: CreateCoffeeDto) {
+  async create(createCoffeeDto: CreateCoffeeDto) {
+    console.time()
+    const rewardsModuleRef = await this.lazyModuleLoader.load(() =>
+      import('../rewards/rewards.module').then((m) => m.RewardsModule),
+    )
+
+    const { RewardsService } = await import('../rewards/rewards.service')
+    const rewardsService = rewardsModuleRef.get(RewardsService)
+    console.timeEnd()
+    rewardsService.grantTo()
+
     return 'This action adds a new coffee'
   }
 
