8c781bf9bf1ead26dd5b215218fe3103c4823f51
egocentryk <lukasz.skowron@gmail.com>
2025-03-06 08:39:56 +0100

    feat(): implementing working thread & piscina workers pool

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { CoffeesModule } from './coffees/coffees.module'
 import { DevtoolsModule } from '@nestjs/devtools-integration'
 import { SchedulerModule } from './scheduler/scheduler.module';
 import { CronModule } from './cron/cron.module';
+import { FibonacciModule } from './fibonacci/fibonacci.module';
 
 @Module({
   imports: [
@@ -14,6 +15,7 @@ import { CronModule } from './cron/cron.module';
     }),
     SchedulerModule,
     CronModule,
+    FibonacciModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/cron/cron.service.ts
+++ src/cron/cron.service.ts
@@ -3,7 +3,7 @@ import { IntervalHost } from '../scheduler/decorators/interval-host.decorator'
 
 @IntervalHost
 export class CronService {
-  @Interval(10000)
+  // @Interval(10000)
   everySecond() {
     console.log('This will be logged every 10 seconds!')
   }
