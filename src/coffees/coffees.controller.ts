051aa9fca89dca21445e7909f7a34a67754bd680
egocentryk <lukasz.skowron@gmail.com>
2025-03-06 11:06:59 +0100

    feat(): implementing the circuit breaker pattern

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,34 +1,47 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { CoffeesService } from './coffees.service';
-import { CreateCoffeeDto } from './dto/create-coffee.dto';
-import { UpdateCoffeeDto } from './dto/update-coffee.dto';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseInterceptors,
+  RequestTimeoutException,
+} from '@nestjs/common'
+import { CoffeesService } from './coffees.service'
+import { CreateCoffeeDto } from './dto/create-coffee.dto'
+import { UpdateCoffeeDto } from './dto/update-coffee.dto'
+import { CircuitBreakerInterceptor } from '../common/interceptors/circuit-breaker/circuit-breaker.interceptor'
 
+@UseInterceptors(CircuitBreakerInterceptor)
 @Controller('coffees')
 export class CoffeesController {
   constructor(private readonly coffeesService: CoffeesService) {}
 
   @Post()
   create(@Body() createCoffeeDto: CreateCoffeeDto) {
-    return this.coffeesService.create(createCoffeeDto);
+    return this.coffeesService.create(createCoffeeDto)
   }
 
   @Get()
   findAll() {
-    return this.coffeesService.findAll();
+    console.log('🦊 "findAll" executed')
+    throw new RequestTimeoutException('💥 Error!')
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.coffeesService.findOne(+id);
+    return this.coffeesService.findOne(+id)
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateCoffeeDto: UpdateCoffeeDto) {
-    return this.coffeesService.update(+id, updateCoffeeDto);
+    return this.coffeesService.update(+id, updateCoffeeDto)
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.coffeesService.remove(+id);
+    return this.coffeesService.remove(+id)
   }
 }
