50d06be16045e9b7b96e55664a99177a948e4803
egocentryk <lukasz.skowron@gmail.com>
2025-03-09 13:25:32 +0100

    feat(): durable providers #2 - i18n

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,9 +13,10 @@ import { PaymentsModule } from './payments/payments.module'
 import { DataSourceModule } from './data-source/data-source.module'
 import { UsersModule } from './users/users.module'
 import { ContextIdFactory } from '@nestjs/core'
-import { AggregateByTenantContextIdStrategy } from './core/aggregate-by-tenant.strategy'
+import { I18nModule } from './i18n/i18n.module'
+import { AggregateByLocaleContextIdStrategy } from './core/aggregate-by-locale.strategy'
 
-ContextIdFactory.apply(new AggregateByTenantContextIdStrategy())
+ContextIdFactory.apply(new AggregateByLocaleContextIdStrategy())
 
 @Module({
   imports: [
@@ -37,6 +38,7 @@ ContextIdFactory.apply(new AggregateByTenantContextIdStrategy())
     PaymentsModule,
     DataSourceModule,
     UsersModule,
+    I18nModule,
     // Alternatively, we can use the `forRootAsync` method to register the module with dynamic options
     // HttpClientModule.forRootAsync({
     //   useFactory: () => ({ baseUrl: 'https://nestjs.com' }),
---
+++ src/core/aggregate-by-locale.strategy.ts
@@ -0,0 +1,47 @@
+import {
+  ContextId,
+  ContextIdFactory,
+  ContextIdResolver,
+  ContextIdResolverFn,
+  ContextIdStrategy,
+  HostComponentInfo,
+} from '@nestjs/core'
+import { pick } from 'accept-language-parser'
+import { Request } from 'express'
+import { I18nService } from 'src/i18n/i18n.service'
+
+export class AggregateByLocaleContextIdStrategy implements ContextIdStrategy {
+  // A collection of context identifiers representing separate DI sub-trees per locale
+  private readonly locales = new Map<string, ContextId>()
+
+  attach(
+    contextId: ContextId,
+    request: Request,
+  ): ContextIdResolverFn | ContextIdResolver {
+    const localeCode =
+      pick(
+        I18nService.supportedLanguages,
+        request.headers['accept-language']!,
+      ) ?? I18nService.defaultLanguage
+
+    let localeSubTreeId: ContextId
+
+    if (this.locales.has(localeCode)) {
+      localeSubTreeId = this.locales.get(localeCode)!
+    } else {
+      // Construct a new context id
+      localeSubTreeId = ContextIdFactory.create()
+      this.locales.set(localeCode, localeSubTreeId)
+
+      // we can remove the locale context id after a certain period of time
+      // if not UserService instance is created once per locale
+      setTimeout(() => this.locales.delete(localeCode), 3000)
+    }
+
+    return {
+      payload: { localeCode },
+      resolve: (info: HostComponentInfo) =>
+        info.isTreeDurable ? localeSubTreeId : contextId,
+    }
+  }
+}
